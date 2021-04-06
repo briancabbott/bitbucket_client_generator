b71f06113b7e65826af03323c68c65b76e3eb95b
Brian <briancabbott@gmail.com>
2021-04-06 02:29:58 -0600

    seperating client from gen.

--- bitbucket_client/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/briancabbott/bitbucket_client
-
-go 1.16
-
-require golang.org/x/oauth2 v0.0.0-20210218202405-ba52d332ba99
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+git clone https://github.com/briancabbott/bitbucket_client
 openapi-generator-cli generate --input-spec ./swagger.json -g go --verbose --config ./client_api_config.yaml --output bitbucket_client
