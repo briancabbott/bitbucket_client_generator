cf466b2c747c7f6b9fc18102fcd515d4f8b505c2
Brian <briancabbott@gmail.com>
2021-04-06 02:17:04 -0600

    working system.

--- build.sh
+++ build.sh
@@ -1,4 +1,13 @@
+# mkdir pkgs 
+cp go.mod ./bitbucket_client
 cd bitbucket_client
-go mod tidy 
-go build ./...
+go mod tidy
+# -v
+# 	print the names of packages as they are compiled.
+# -work
+# 	print the name of the temporary work directory and
+# 	do not delete it when exiting.
+# -x
+# 	print the commands. 
+go build -v -work -x -a ./...
 cd ..
